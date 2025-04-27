07d4867aefa2934889190815565148c0de2ddde2
Darren Barklie <darrenbarklie@gmail.com>
2025-04-27 13:25:08 +0100

    Reconfigure favicon integration, add favicon.svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
-
-import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
+import favicons from 'astro-favicons';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gdgdouglas.com',
-  integrations: [sitemap(), tailwind(), solidJs(), mdx()],
+  integrations: [sitemap(), tailwind(), solidJs(), mdx(), favicons()],
   output: 'server',
 
   adapter: node({
